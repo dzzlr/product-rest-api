b90df1c12bbccc4bf6985bd1778d91282dc952e6
dzzlr <githubdazzler@gmail.com>
2023-04-21 10:04:28 +0700

    Add: cors

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-const port = 8000
-const db = require('./src/configs/db.config')
-const response = require('./src/utils/response')
+const port = 8000;
+const db = require('./src/configs/db.config');
+const response = require('./src/utils/response');
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get('/', (req, res) => {
-    response(200, 'Welcome to RESTful API Product Express', res)
-})
+    response(200, 'Welcome to RESTful API Product Express', res);
+});
 
-app.get('/product', (req, res) => {
-    let name = req.query.name || '';
-    let sortby = req.query.sortby || '';
-
-    response(200, `Query name is ${name} and SortBy is ${sortby}`, res)
-})
-
-require('./src/routes/product.routes')(app)
+require('./src/routes/product.routes')(app);
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`)
-})
+    console.log(`Server is running on http://localhost:${port}`);
+});

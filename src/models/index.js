e7469619a9ca0fbb5d4aa558246c87f6d8077c01
dzzlr <githubdazzler@gmail.com>
2023-04-21 10:02:40 +0700

    refactor code and fixing product model

--- src/models/index.js
+++ src/models/index.js
@@ -1,13 +1,13 @@
-const dbConfig = require('../configs/db.config')
-const mongoose = require('mongoose')
+const dbConfig = require('../configs/db.config');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.Promise = global.Promise
+mongoose.Promise = global.Promise;
 
-const db = {}
-db.mongoose = mongoose
+const db = {};
+db.mongoose = mongoose;
 // db.url = "mongodb://127.0.0.1:27017/collect_product"
-db.url = process.env.DATABASE_URL
-db.products = require('./product.model')(mongoose)
+db.url = process.env.DATABASE_URL;
+db.products = require('./product.model')(mongoose);
 
-module.exports = db
+module.exports = db;
--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,26 +1,26 @@
 module.exports = (mongoose) => {
     const schema = mongoose.Schema(
         {
-            sku: String,
-            brand: String,
-            name: String,
-            edition: String,
-            color: String,
-            price: Number,
-            size: Object,
-            image: Object,
-            url: String,
+            sku: {type: String, required: true, unique: true},
+            brand: {type: String, required: true},
+            name: {type: String, required: true},
+            edition: {type: String, required: false},
+            color: {type: String, required: false},
+            price: {type: Number, required: true},
+            // size: Object,
+            // image: Object,
+            url: {type: String, required: true},
             // description: String,
         },
         { timestamps: true }
-    )
+    );
 
-    schema.method('toJSON', function() {
-        const {__v, _id, ...object} = this.toObject()
-        object.id = _id
-        return object
-    })
+    // schema.method('toJSON', function() {
+    //     const {__v, _id, ...object} = this.toObject()
+    //     object.id = _id
+    //     return object
+    // })
 
-    const Product = mongoose.model("products", schema)
-    return Product
+    const Product = mongoose.model("products", schema);
+    return Product;
 }

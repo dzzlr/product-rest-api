4d0c81e6aaf13d4fd2a1487a2010983b38790986
dzzlr <githubdazzler@gmail.com>
2023-04-21 10:06:41 +0700

    Adding features: pagination and filter, refactor code

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,88 +1,142 @@
-const db = require('../models')
-const Product  = db.products
+const db = require('../models');
+const Product = db.products;
 
-exports.findAll = (req, res) => {
-    Product.find()
-        .then((result) => {
-            res.send(result)
-        }).catch((err) => {
-            res.status(500).send({
-                message: err.message || "Some error while retrieving products."
-            })
+exports.findAll = async (req, res) => {
+    const name = req.query.name;
+    const brand = req.query.brand || ['Adidas', 'New Balance', 'Nike'];
+    const price = req.query.price || 0;
+
+    const page = parseInt(req.query.page) - 1 || 0;
+    const limit = parseInt(req.query.limit) || 5;
+
+    try {
+        const result = await Product.find({
+            brand: brand,
+            price: { $gt: price } 
+        })
+        .skip(page * limit)
+        .limit(limit);
+
+        res.status(200).send({
+            code: 200,
+            status: "OK",
+            data: result,
+            metadata: {
+                page: page + 1,
+                limit: limit,
+                // current: "",
+                // prev: "",
+                // next: "",
+            }
         });
+    } catch (error) {
+        res.status(500).send({
+            message: error.message || "Some error while retrieving products."
+        });
+    }
 }
 
-exports.create = (req, res) => {
-    const product = new Product({
-        title: req.body.title,
+exports.create = async (req, res) => {
+    const product = await new Product({
+        sku: req.body.sku,
         brand: req.body.brand,
-        description: req.body.description ? req.body.description : "null"
-    })
+        name: req.body.name,
+        edition: req.body.edition ? req.body.edition : null,
+        color: req.body.color ? req.body.color : null,
+        price: req.body.price,
+        url: req.body.url,
+        // description: req.body.description ? req.body.description : "null"
+    });
 
-    product.save(product)
-        .then((result) => {
-            res.send(result)
-        }).catch((err) => {
-            res.status(409).send({
-                message: err.message || "Some error while create products."
-            })
+    try {
+        const savedProduct = await product.save();
+        res.status(200).send({
+            code: 200,
+            status: "OK",
+            data: savedProduct
         });
+    } catch (error) {
+        res.status(409).send({
+            message: error.message || "Some error while create products."
+        });
+    }
 }
 
-exports.findOne = (req, res) => {
-    const id = req.params.id
+exports.findOne = async (req, res) => {
+    const id = req.params.id;
 
-    Product.findById(id)
-        .then((result) => {
-            if (!result) {
-                res.status(404).send({
-                    message: "Product not found"
-                })
-            }
-            res.send(result)
-        }).catch((err) => {
-            res.status(409).send({
-                message: err.message || "Some error while show product."
-            })
+    try {
+        const result = await Product.findById(id);
+
+        if (!result) {
+            res.status(404).send({
+                code: 404,
+                status: "NOT_FOUND",
+                message: "Product not found"
+            });
+        }
+
+        res.status(200).send({
+            code: 200,
+            status: "OK",
+            data: result
         });
+    } catch (error) {
+        res.status(409).send({
+            message: error.message || "Some error while show product."
+        });
+    }
 }
 
-exports.update = (req, res) => {
-    const id = req.params.id
+exports.update = async (req, res) => {
+    const id = req.params.id;
 
-    Product.findOneAndUpdate(id, req.body)
-        .then((result) => {
-            if (!result) {
-                res.status(404).send({
-                    message: "Product not found"
-                })
-            }
-            res.send({
-                message: "Product was updated"
-            })
-        }).catch((err) => {
-            res.status(409).send({
-                message: err.message || "Some error while update product."
-            })
+    try {
+        const result = await Product.findByIdAndUpdate(id, req.body);
+
+        if (!result) {
+            res.status(404).send({
+                code: 404,
+                status: "NOT_FOUND",
+                message: "Product not found"
+            });
+        }
+
+        res.status(200).send({
+            code: 200,
+            status: "OK",
+            message: "Product was updated",
+            data: result
         });
+    } catch (error) {
+        res.status(409).send({
+            message: error.message || "Some error while update product."
+        });
+    }
 }
 
-exports.delete = (req, res) => {
-    const id = req.params.id
+exports.delete = async (req, res) => {
+    const id = req.params.id;
 
-    Product.findOneAndRemove(id)
-        .then((result) => {
-            if (!result) {
-                res.status(404).send({
-                    message: "Product not found"
-                })
-            }
-            res.send({
-                message: "Product was deleted"
-            })
-        }).catch((err) => {
-            res.status(409).send({
-                message: err.message || "Some error while delete product."
-            })
+    try {
+        const result = await Product.findByIdAndDelete(id);
+
+        if (!result) {
+            res.status(404).send({
+                code: 404,
+                status: "NOT_FOUND",
+                message: "Product not found"
+            });
+        }
+
+        res.status(200).send({
+            code: 200,
+            status: "OK",
+            message: "Product was deleted"
         });
+    } catch (error) {
+        res.status(409).send({
+            message: error.message || "Some error while delete product."
+        });
+    }
 }
